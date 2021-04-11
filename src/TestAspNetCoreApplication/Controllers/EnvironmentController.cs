fa8b472c04c25ae91b152baab41c058eeba92eb6
CodeFuller <CodeFuller@gmail.com>
2021-04-11 12:46:05 +0300

    Add Environment controller

---
+++ src/TestAspNetCoreApplication/Controllers/EnvironmentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TestAspNetCoreApplication.Models;
+
+namespace TestAspNetCoreApplication.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class EnvironmentController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<EnvironmentVariable> Get()
+		{
+			return Environment.GetEnvironmentVariables()
+				.Cast<DictionaryEntry>()
+				.Select(x => new { Name = (string)x.Key, Value = (string)x.Value })
+				.Select(x => new EnvironmentVariable
+				{
+					Name = x.Name,
+					Value = x.Value,
+				});
+		}
+	}
+}
---
+++ src/TestAspNetCoreApplication/Models/EnvironmentVariable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace TestAspNetCoreApplication.Models
+{
+	[DataContract]
+	public class EnvironmentVariable
+	{
+		[DataMember(Name = "name")]
+		public string Name { get; init; }
+
+		[DataMember(Name = "value")]
+		public string Value { get; init; }
+	}
+}

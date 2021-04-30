abebfb5d9f8375849086b4e08636b933e0949f5d
CodeFuller <CodeFuller@gmail.com>
2021-04-30 09:18:05 +0300

    Add integration tests

---
+++ tests/TestAspNetCoreApplication.IntegrationTests/Controllers/EnvironmentControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestAspNetCoreApplication.Models;
+
+namespace TestAspNetCoreApplication.IntegrationTests.Controllers
+{
+	[TestClass]
+	public class EnvironmentControllerTests
+	{
+		[TestMethod]
+		public async Task Get_ReturnsCorrectResponse()
+		{
+			// Arrange
+
+			Environment.SetEnvironmentVariable("TEST_ENVIRONMENT_VARIABLE", "Some Test Value");
+
+			using var factory = new WebApplicationFactory<Startup>();
+			var client = factory.CreateClient();
+
+			// Act
+
+			var variables = await client.GetFromJsonAsync<IReadOnlyCollection<EnvironmentVariable>>("/environment", CancellationToken.None);
+
+			// Assert
+
+			variables.Should().ContainSingle(x => x.Name == "TEST_ENVIRONMENT_VARIABLE" && x.Value == "Some Test Value");
+		}
+	}
+}
